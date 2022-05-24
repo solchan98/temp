7e4c874f59a579be681191cbd2fd7108ee3a6ae3
solchan98 <dev.sol0427@gmail.com>
2022-05-25 01:52:53 +0900

    refactor: trendData 요청 시, 이전 데이터도 함께 포함하여 응답하도록 수정

--- src/daily-report/daily-report.controller.ts
+++ src/daily-report/daily-report.controller.ts
@@ -1,5 +1,26 @@
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { DailyReportService } from './daily-report.service';
+import * as dayjs from 'dayjs';
+
+interface IDate {
+  startDate: Date;
+  endDate: Date;
+}
+
+export const getPrevDateByCurDate = (startDate, endDate): IDate => {
+  const DATE_FORM = 'YYYY-MM-DD';
+  const diffDay = dayjs(endDate).diff(startDate, 'd') + 1;
+  const prevStartDate = dayjs(startDate)
+    .subtract(diffDay, 'd')
+    .format(DATE_FORM);
+  const prevEndDate = dayjs(prevStartDate)
+    .add(diffDay - 1, 'd')
+    .format('YYYY-MM-DD');
+  return {
+    startDate: new Date(prevStartDate),
+    endDate: new Date(prevEndDate),
+  };
+};
 
 @Controller('/api/v1/daily-report')
 export class DailyReportController {
@@ -10,7 +31,16 @@ export class DailyReportController {
     @Query('startDate') startDate: Date,
     @Query('endDate') endDate: Date,
   ) {
-    return await this.service.getDailyReportByDate(startDate, endDate);
+    const prevDate = getPrevDateByCurDate(startDate, endDate);
+    const curData = await this.service.getDailyReportByDate(startDate, endDate);
+    const prevData = await this.service.getDailyReportByDate(
+      prevDate.startDate,
+      prevDate.endDate,
+    );
+    return {
+      curData,
+      prevData,
+    };
   }
 
   @Post('')
